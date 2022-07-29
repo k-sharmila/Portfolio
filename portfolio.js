7389800cfc9891ef5a75d4acad7bac6f1942bfab
Sharmila <96688536+k-sharmila@users.noreply.github.com>
2022-07-29 06:34:39 -0700

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -2,7 +2,6 @@
 var navbar = document.querySelector(".navbar");
 // when the scroll is higher than 20 viewport height, add the sticky classs to the tag with a class navbar
 window.onscroll = () => {
-  
   this.scrollY > 20
     ? navbar.classList.add("sticky")
     : navbar.classList.remove("sticky");
